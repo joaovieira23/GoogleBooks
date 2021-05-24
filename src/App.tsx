84bd8ba66a4be7ec4bf08a30f86197dc31f24e91
João Victor Vieira de Andrade <joaovictorvieira.23@hotmail.com>
2021-05-24 14:41:55 -0300

    feat: define header and inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './App.css';
+import { InputGroup, Input, } from 'reactstrap';
 
 function App() {
+  const mainHeader = () => {
+    return (
+      <div className="main-image d-flex justify-content-center align-items-center flex-column">
+        <div className="filter">
+          <h1 style={{ fontFamily: 'Poppins', color: '#CDCDCD' }} className="display-2 text-center text-white ab-3">
+            Google Books
+          </h1>
+          <div style={{ width: '60%' }}>
+            <InputGroup size="1g" className="mb-3">
+              <Input placeholder="Busque seus livros" />
+            </InputGroup>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <h1>Hello Google Books</h1>
+      {mainHeader()}
     </div>
   );
 }
